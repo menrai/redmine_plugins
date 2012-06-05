e163adb2e1b87b0fe198c70727f00bd789536ced
menrai <menrai-s@ijk.com>
2012-06-05 12:51:37 +0000

    first commit

---
+++ redmine_knowledgebase/app/controllers/articles_controller.rb
@@ -0,0 +1,123 @@
+class ArticlesController < KnowledgebaseController
+  unloadable
+  
+  helper :attachments
+  include AttachmentsHelper
+  include FaceboxRender
+
+  #Authorize against global permissions defined in init.rb
+  before_filter :authorize_global
+  
+  def new
+    @article = KbArticle.new
+    @default_category = params[:category_id]
+    @article.category_id = params[:category_id]
+  end
+  
+  def rate
+    @article = KbArticle.find(params[:article_id])
+    @article.rate params[:rating].to_i
+    render :partial => "rating", :locals => {:article => @article}
+  end
+  
+  def create    
+    @article = KbArticle.new(params[:article])
+    @article.category_id = params[:category_id]
+    @article.author_id = User.current.id
+    @article.project_id = 1
+    if @article.save
+      attachments = attach(@article, params[:attachments])
+      flash[:notice] = l(:label_article_created, :title => @article.title)
+      redirect_to({ :controller => 'knowledgebase', :action => 'index' })
+    else
+      render(:action => 'new')
+    end
+  end
+  
+  def show
+    @article = KbArticle.find(params[:id] || params[:article_id])
+    @article.view request.remote_addr, User.current
+    @attachments = @article.attachments.find(:all, :order => "created_on DESC")
+    @comments = @article.comments
+  end
+  
+  def edit
+    @article = KbArticle.find(params[:id])
+  end
+  
+  def update
+    @article = KbArticle.find(params[:id])
+    params[:article][:category_id] = params[:category_id]
+    if @article.update_attributes(params[:article])
+      attachments = attach(@article, params[:attachments])
+      flash[:notice] = l(:label_article_updated)
+      redirect_to({ :action => 'show', :id => @article.id })
+    else
+      render({:action => 'edit', :id => @article.id})
+    end
+  end
+  
+  def add_comment
+    @article = KbArticle.find(params[:id])
+    @comment = Comment.new(params[:comment])
+    @comment.author = User.current || nil
+    if @article.comments << @comment
+      flash[:notice] = l(:label_comment_added)
+      redirect_to :action => 'show', :id => @article
+    else
+      show
+      render :action => 'show'
+    end
+  end
+
+  def destroy_comment
+    @article = KbArticle.find(params[:id])
+    @article.comments.find(params[:comment_id]).destroy
+    redirect_to :action => 'show', :id => @article
+  end
+  
+  def destroy
+    @article = KbArticle.find(params[:id])
+    @article.destroy
+    flash[:notice] = l(:label_article_removed)
+    redirect_to({ :controller => 'knowledgebase', :action => 'index' })
+  end
+
+  def add_attachment
+    @article = KbArticle.find(params[:id])
+    attachments = attach(@article, params[:attachments])    
+    redirect_to({ :action => 'show', :id => @article.id })
+  end
+  
+  def tagged
+    @tag = params[:id]
+    @list = KbArticle.find_tagged_with(@tag)
+  end
+
+  def preview
+    @summary = (params[:article] ? params[:article][:summary] : nil)
+    @content = (params[:article] ? params[:article][:content] : nil)
+    render :layout => false
+  end
+  
+  def comment
+    @article_id = params[:article_id]
+    render_to_facebox
+  end
+
+#######
+private
+#######
+
+  # Abstract attachment method to resolve how files should be attached to a model.
+  # In newer versions of Redmine, the attach_files functionality was moved
+  # from the application controller to the attachment model.
+  def attach(target, attachments)
+    if Attachment.respond_to?(:attach_files)
+      Attachment.attach_files(target, attachments)
+      render_attachment_warning_if_needed(target)
+    else
+      attach_files(target, attachments)
+    end
+  end
+end
---
+++ redmine_knowledgebase/app/models/kb_article.rb
@@ -0,0 +1,48 @@
+class KbArticle < ActiveRecord::Base
+  unloadable
+
+  set_table_name "kb_articles"
+
+  validates_presence_of :title
+  validates_presence_of :category_id
+
+  belongs_to :project # XXX association added to allow searching to work
+  belongs_to :category, :class_name => "KbCategory"
+  belongs_to :author,   :class_name => 'User', :foreign_key => 'author_id'
+
+  acts_as_viewed
+  acts_as_rated :no_rater => true
+  acts_as_taggable
+  acts_as_attachable
+
+  acts_as_searchable :columns => [ "kb_articles.title", "kb_articles.content"],
+                     :include => [ :project ],
+                     :order_column => "kb_articles.id",
+                     :permission => nil
+
+  acts_as_event :title => Proc.new { |o| "#{l(:label_title_articles)} ##{o.id}: #{o.title}" },
+                :description => Proc.new { |o| "#{o.content}" },
+                :datetime => :created_at,
+                :type => 'articles',
+                :url => Proc.new { |o| {:controller => 'articles', :action => 'show', :id => nil, :article_id => o.id} }
+
+  has_many :comments, :as => :commented, :dependent => :delete_all, :order => "created_on"
+
+  # This overrides the instance method in acts_as_attachable
+  def attachments_visible?(user=User.current)
+    true
+  end
+
+  def attachments_deletable?(user=User.current)
+    user.logged?
+  end
+
+  # XXX this is required by acts_as_attachable. Without this, trying to download
+  # a file throws the following:
+  # "NoMethodError (undefined method 'project' for #(ActiveRecord::Associations::BelongsToPolymorphicAssociation))"
+  def project
+    nil
+  end
+
+end
+
